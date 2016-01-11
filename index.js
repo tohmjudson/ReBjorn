e42cb023f4f04937b7ec774e79cfd7c5fab558f9
Tohm Judson <tohm@tohmjudson.com>
2016-01-10 20:27:17 -0700

    updated server for Heroku

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var path = require('path');
+var port_number = http.listen(process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -9,10 +10,8 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-var port_number = server.listen(process.env.PORT || 3000);
 app.listen(port_number);
 
 // http.listen(3000, function(){
 //   console.log('listening on *:3000');
 // });
-
